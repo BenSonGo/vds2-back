839e99768b1b6d1a3b9057593ec3a8ceac604c71
Anton Ivannyk <a.ivannyk@deltafunc.com>
2023-02-21 20:49:29 -0500

    generate users password command

---
+++ app/Console/Commands/GenerateUsersPasswordsCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class GenerateUsersPasswordsCommand extends Command
+{
+    protected $signature = 'generate:users_passwords';
+
+    protected $description = 'Command description';
+
+    public function handle(): int
+    {
+        $users = User::all();
+
+        /** @var User $user */
+        foreach ($users as $user) {
+            $user->password = Hash::make('123456789' . $user->id);
+            $user->save();
+        }
+
+        return Command::SUCCESS;
+    }
+}
